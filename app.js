d7c43c4f24882314c23ea186ffddc0b4504eac2c
muunji <twoji3019@naver.com>
2025-03-23 16:03:24 +0900

    dotenv 설치

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
 import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
+//환경변수
+dotenv.config()
 
 const app = express()
 
