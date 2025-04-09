307d37d0ed869adf35bf92c03d4182672b31c132
muunji <twoji3019@naver.com>
2025-04-09 22:18:05 +0900

    input 생성 함수 작성

--- src/say.js
+++ src/say.js
@@ -2,6 +2,16 @@
 
 //2. 구매처 구매 날짜 선택 함수
 
+
+//3.4 에 동일한 구성 필요
+function makeInput(inputName){
+  let input = document.createElement('input')
+  input.type = 'text'
+  input.name = inputName
+
+  return input
+}
+
 //3. 리스트 작성 함수
 
 //4. 리스트 수정, 삭제 함수
