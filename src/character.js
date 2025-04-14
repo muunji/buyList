fb01622aad725680fd8342662df3e5fee300fa59
muunji <twoji3019@naver.com>
2025-04-14 22:02:27 +0900

    함수 이름 지정

--- src/character.js
+++ src/character.js
@@ -5,7 +5,16 @@
 // four: dodgerblue;
 
 //색깔이 있는 div
+function makeColorDiv(number){
+
+}
 
 //색깔이 있는 div를 가진 요소 생성 함수
+function sectionColorDiv(){
+
+}
 
 //div 선택했을 때 반환값을 가지는 함수
+function selectColorDiv(){
+  
+}
