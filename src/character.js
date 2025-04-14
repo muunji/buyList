ee1b44316480be7023bdf0662cc6b97cc9cce37f
muunji <twoji3019@naver.com>
2025-04-14 22:04:29 +0900

    클래스 이름을 배열로 지정 + 색깔이 있는 div 만드는 함수 작성

--- src/character.js
+++ src/character.js
@@ -4,9 +4,16 @@
 // three: darkgreen;
 // four: dodgerblue;
 
+colorArr = ['one','two','three','four']
+
 //색깔이 있는 div
 function makeColorDiv(number){
+  let colorDiv = document.createElement('div')
+  colorDiv.style.width = '200px'
+  colorDiv.style.height='200px'
+  colorDiv.style.borderRadius = '50%'
 
+  colorDiv.classList.add = colorArr[number]
 }
 
 //색깔이 있는 div를 가진 요소 생성 함수
@@ -16,5 +23,5 @@ function sectionColorDiv(){
 
 //div 선택했을 때 반환값을 가지는 함수
 function selectColorDiv(){
-  
+
 }
