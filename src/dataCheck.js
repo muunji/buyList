6ab363d4c43120c4a37f3deefa36025ee0807463
muunji <twoji3019@naver.com>
2025-04-15 22:37:53 +0900

    List 생성자 작성 중

--- src/dataCheck.js
+++ src/dataCheck.js
@@ -29,3 +29,39 @@ class Day {
 }
 
 //category, item, number, price에 대한 유효성 검사
+class List{
+  constructor(category, item, number, price){
+    this.category = category
+    this.item = item
+    this.number = number
+    this.price = price
+  }
+
+  set category(category){
+    //값이 있으면
+    if(category){
+      this._category = category
+    }
+  }
+
+  set item(item){
+    //문자열이면
+    if(typeof item === 'string'){
+      this._item = item
+    }
+  }
+
+  set number(number){
+    //수량, 숫자로 적힌 내용이 있으면
+    if(!isNaN(Number(number))){
+      this._number=Number
+    }
+  }
+
+  set price(price){
+    //가격, 숫자로 적힌 내용이 있으면
+    if(!isNaN(Number(price))){
+      this._price=price
+    }
+  }
+}
