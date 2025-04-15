19bcadd82864e792459a8006b59abdb41bfd253a
muunji <twoji3019@naver.com>
2025-04-15 22:33:26 +0900

    Day 클래스 생성

--- src/dataCheck.js
+++ src/dataCheck.js
@@ -1,3 +1,31 @@
 //date, where에 대한 유효성 검사
+class Day {
+  constructor(date, where){
+    this.date = date;
+    this.where=where;
+  }
+
+  set date(date){
+    //값이 있으면
+    if(date) {
+      this._date = date
+    }
+  }
+
+  set where(where){
+    //문자열이면
+    if(typeof where === 'string'){
+      this._where=where;
+    }
+  }
+
+  get date(){
+    return this._date
+  }
+
+  get where(){
+    return this._where
+  }
+}
 
 //category, item, number, price에 대한 유효성 검사
